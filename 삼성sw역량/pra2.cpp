0aa180a34754bf40f75517af9f770b3649b88baf
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-10-25 19:55:22 +0900

    2020 kakao string 문자열 관련 정리 필요

--- 삼성sw역량/pra2.cpp
+++ 삼성sw역량/pra2.cpp
@@ -1,25 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
-vector<int> profit;
-int T[16]; int P[16]; int c[16];
-int N;
-vector<pair<int,int>> v(4);
-void route_left(){
-    int a = v[3].first;
-    int b = v[3].second;
-    v[3] = v[2];
-    v[2] = v[1];
-    v[1] = v[0];
-    v[0].first = a;
-    v[0].second = b;
-}
 
 int main(){
-    v = {{-1,0}, {0,1}, {0,1}, {0,-1}};
-    route_left();
-
-    cout <<v[0].first << ' ' << v[0].second << endl;
-
+    string s = "abcd";
+    string s1 = "a";
+    cout << s[3] << endl;
     return 0;
 }
