1630c86a9c9db1cf79b603a00eb41b3cea0fdd7a
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-18 20:07:25 +0900

    type: 구현

---
+++ Programmers/level2/Draw.py
@@ -0,0 +1,9 @@
+def solution(n,a,b):
+    answer = 1
+    while True:
+        if (a//2 + a%2) == (b//2 + b%2): break
+        a = a // 2 + a%2
+        b = b // 2 + b%2
+        answer+=1
+
+    return answer
