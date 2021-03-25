b5089fabb06ed84d38b10087c078faaac02b46df
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-03-25 11:21:35 +0900

    refactor: 2018 KAKAO BLIEND

--- Programmers/kakao/cache.py
+++ Programmers/kakao/cache.py
@@ -1,21 +1,22 @@
-def solution(cacheSize, cities):
+from collections import deque
+
+
+def solution(cache_size, cities):
     answer = 0
+    if cache_size == 0:
+        return len(cities)*5
     cache = []
-    low_cities = []
-    for city in cities: low_cities.append(city.lower())
-    if cacheSize == 0: return 5*len(cities)
-    for city in low_cities:
+    cities = list(map(lambda x: x.lower(), cities))
+    for city in cities:
+        l_cache = len(cache)
         if city in cache:
-            i = cache.index(city)
-            pre, post = cache[:i], cache[i+1:]
-            cache = pre+post
+            cache.remove(city)
             cache.append(city)
-            answer+=1
-            continue
-            
-        if len(cache) == cacheSize:
-            cache.pop(0)
-        cache.append(city)
-        answer+=5
-        
+            answer += 1
+        else:
+            if l_cache >= cache_size:
+                cache.pop(0)
+            cache.append(city)
+            answer += 5
+
     return answer
