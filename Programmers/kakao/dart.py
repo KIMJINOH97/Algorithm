ed55decd5a34912ef69c2153a85878801e8a1204
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-03-26 13:20:53 +0900

    refactor: 2018 KAKAO BLIND

--- Programmers/kakao/dart.py
+++ Programmers/kakao/dart.py
@@ -2,26 +2,25 @@ import re
 
 
 def solution(dartResult):
-    p = re.compile('[0-9]+|[#*SDT]')
-    da = p.findall(dartResult)
-    answer = 0
-    pre = 0
-    num = 0
-    for dart in da:
-        if dart.isdigit():
-            pre = num
-            answer += num
-            num = int(dart)
-        else:
-            if dart == 'D':
-                num = pow(num, 2)
-            elif dart == 'T':
-                num = pow(num, 3)
-            elif dart == '#':
-                num = -num
-            elif dart == '*':
-                answer -= pre
-                answer += pre*2
-                num *= 2
+    p = re.compile('[0-9]+|[SDT*#]')
+    dart = p.findall(dartResult)
+    answer = []
 
-    return answer+num
+    for i, d in enumerate(dart):
+        if d.isdigit():
+            if i != 0:
+                answer.append(num)
+            num = int(d)
+        else:
+            if d == 'D':
+                num = pow(num, 2)
+            elif d == 'T':
+                num = pow(num, 3)
+            elif d == '*':
+                num *= 2
+                if len(answer) > 0:
+                    answer[-1] *= 2
+            elif d == '#':
+                num *= -1
+
+    return sum(answer)+num
