2d7c868eff7dac003af7c907d194a7df516ec644
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-03-27 22:15:13 +0900

    refactor: 2018 KAKAO BLIIND

--- Programmers/kakao/compression.py
+++ Programmers/kakao/compression.py
@@ -1,22 +1,16 @@
 def solution(msg):
     answer = []
-    dic = {}
-    for i in range(26): dic[chr(ord('A')+i)] = i+1
-    lzw,i, num = 0,0,26
-    while len(msg) != lzw:
-        add = msg[i]
-        for j in range(i+1, len(msg)):
-            add+=msg[j]
-            if add not in dic: # 긴문자열 찾았을때
-                num+=1
-                dic[add] = num # 긴문자열 색인번호 등록
-                lzw += len(add)-1 # 입력에서 제거
-                answer.append(dic[add[:-1]]) # 색인번호 출력
-                i += j-i
-                break
+    dic = {chr(65+i): i+1 for i in range(26)}
+
+    index = 27
+    str1 = ""
+    for m in msg:
+        if str1+m in dic:
+            str1 += m
         else:
-            lzw += len(add)
-            answer.append(dic[add])
-                
-    
-    return answer
+            dic[str1+m] = index
+            index += 1
+            answer.append(dic[str1])
+            str1 = m
+
+    return answer+[dic[str1]]
