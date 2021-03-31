2fdcc14a73a5198158402185ffa9e640ef52f109
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-03-31 19:12:48 +0900

    refactor: 2020 KAKAO INTERN

--- Programmers/kakao/jwely_shopping.py
+++ Programmers/kakao/jwely_shopping.py
@@ -1,32 +1,26 @@
 def solution(gems):
     answer = []
-    dia = set(gems)
-    check = {j: 0 for j in dia}
+    gem = set(gems)
+    gem_cnt = len(gem)
+    check = {g: 0 for g in gem}
 
     l, r = 0, 0
-    check[gems[l]] += 1
     cnt = 1
+    check[gems[0]] = 1
 
     while l <= r:
-        if r == len(gems)-1 and r-l+1 < len(dia):
-            break
-        if cnt == len(dia):
+        if cnt == gem_cnt:
             answer.append([l+1, r+1])
-            check[gems[l]] -= 1
-            if check[gems[l]] == 0:
+            if check[gems[l]] == 1:
                 cnt -= 1
+            check[gems[l]] -= 1
             l += 1
-        else:
-            if r != len(gems) - 1:
-                r += 1
-                if check[gems[r]] == 0:
-                    cnt += 1
-                check[gems[r]] += 1
-            else:
-                check[gems[l]] -= 1
-                if check[gems[l]] == 0:
-                    cnt -= 1
-                l += 1
-
-    answer = sorted(answer, key=lambda x: (x[1]-x[0], x[0], x[1]))
+        elif cnt < gem_cnt:
+            r += 1
+            if r >= len(gems):
+                break
+            if check[gems[r]] == 0:
+                cnt += 1
+            check[gems[r]] += 1
+    answer.sort(key=lambda x: (x[1]-x[0], x[0]))
     return answer[0]
