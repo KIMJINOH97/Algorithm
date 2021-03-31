51cafc62fde77760d0ea71cd3048f3cf133017bc
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-03-31 11:14:07 +0900

    refactor: 2019 KAKAO BLIND

--- Programmers/kakao/candidate_key.py
+++ Programmers/kakao/candidate_key.py
@@ -1,38 +1,33 @@
-column = [] #검사할 열
-check = [0 for i in range(8)]
+from itertools import combinations
 
-def combi(n, result, c_l, start, count):
-    if count == n:
-        column.append(result)
-        return
-    for i in range(start, c_l):
-        if check[i] == 0:
-            check[i] = 1
-            combi(n, result+str(i), c_l, i+1, count+1)
-            check[i] = 0
 
 def solution(relation):
-    answer = 0
-    column_len = len(relation[0])
-    least = [] # 최소성 만족하는 집합
-    for i in range(1, column_len+1):
-        combi(i, "", column_len, 0, 0)
-    print(column)
-    for st in column:
-        li = []
-        flag = False
-        for j in least:
-            #if j in st: break
-            a, b = set(j), set(st)
-            if a&b == a: break  
-        else:        
-            for r in relation:
-                str1 = ""
-                for s in st: str1 += r[int(s)]
-                li.append(str1)
-            copy = list(set(li))
-            if len(copy) == len(relation):
-                least.append(st)
-                answer+=1
-            
-    return answer
+    answer = []
+    l_tuple, l_relation = len(relation[0]), len(relation)
+    key = [i for i in range(l_tuple)]
+    all_keys = []
+    for i in range(1, l_tuple+1):
+        all_keys.append(list(combinations(key, i)))
+
+    for keys in all_keys:
+        for key in keys:
+            same = 0
+            # 부분 집합 판별
+            s_key = set(key)
+            for a in answer:
+                s_a = set(a)
+                if s_key & s_a == s_a:
+                    same = 1
+                    break
+            if same:
+                continue
+
+            cmp = ["" for i in range(l_relation)]
+            for k in key:
+                for j in range(l_relation):
+                    cmp[j] += relation[j][k]
+
+            if len(set(cmp)) == l_relation:
+                answer.append(key)
+
+    return len(answer)
