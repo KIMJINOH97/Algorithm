a0fa772cf63d373f4cfe1fb92e362e8546920536
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-04-06 22:53:31 +0900

    refactor: 2021 KAKAO BLIND

--- Programmers/kakao/menu_renewer.py
+++ Programmers/kakao/menu_renewer.py
@@ -1,41 +1,33 @@
+from itertools import combinations
+
+
 def solution(orders, course):
     answer = []
-    order_len = [len(m) for m in orders]
-    order_max = max(order_len)
+    order_list = []
+    for order in orders:
+        menu = list(order)
+        for c in course:
+            if c > len(order):
+                continue
+            for com in combinations(order, c):
+                arr = ''.join(sorted(list(com)))
+                order_list.append(arr)
+
     dic = {}
-    check = [0 for i in range(10)]
-    for i, order in enumerate(orders):
-        orders[i] = sorted(order)
+    order_len = [0 for i in range(11)]
+    for order in order_list:
+        if order not in dic:
+            dic[order] = 1
+        else:
+            dic[order] += 1
+        o_len = len(order)
+        if dic[order] > order_len[o_len]:
+            order_len[o_len] = dic[order]
 
-    def combi(n, ans, start, row):
-        if len(ans) == n:
-            if ans not in dic:
-                dic[ans] = 1
-            else:
-                dic[ans] += 1
-            return 0
-        for i in range(start, len(orders[row])):
-            if check[i] == 0:
-                check[i] = 1
-                combi(n, ans+orders[row][i], i+1, row)
-                check[i] = 0
+    for key in dic:
+        k_len = len(key)
+        if order_len[k_len] > 1 and order_len[k_len] == dic[key]:
+            answer.append(key)
 
-    for c in course:
-        for j in range(len(orders)):
-            if c <= order_max:
-                combi(c, "", 0, j)
-    c_max = []
-    for c in course:
-        count_max = 0
-        for key in dic:
-            if len(key) == c and dic[key] > count_max:
-                count_max = dic[key]
-        c_max.append(count_max)
-
-    for i, count in enumerate(c_max):
-        for key in dic:
-            if dic[key] == count and count >= 2 and len(key) == course[i]:
-                answer.append(key)
     answer.sort()
-
     return answer
