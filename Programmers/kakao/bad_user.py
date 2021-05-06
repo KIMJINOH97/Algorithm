d8b0577edf68d511fbc620bcd09a44b46c194457
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-05-06 15:54:23 +0900

    refactor: 2019 KAKAO INTERN

--- Programmers/kakao/bad_user.py
+++ Programmers/kakao/bad_user.py
@@ -1,56 +1,34 @@
-from itertools import permutations
+from itertools import permutations, combinations
 
 
 def solution(user_id, banned_id):
-    global answer
     answer = 0
-    check = [0 for i in range(len(user_id))]
-    ban_check = {user: 0 for user in user_id}
-    c = []
+    B = len(banned_id)
+    com_list = list(combinations(user_id, B))
 
-    def match(user, ban):
+    def is_match(user, ban):
         if len(user) != len(ban):
             return False
-        for i in range(len(ban)):
-            if ban[i] == '*' or ban[i] == user[i]:
+        for u, b in zip(user, ban):
+            if b == "*":
+                continue
+            elif u == b:
                 continue
             else:
                 return False
         return True
 
-    def right(arr):
-        for ban in banned_id:
-            for a in arr:
-                if ban_check[a] == 0 and match(a, ban):
-                    ban_check[a] = 1
-                    break
-            else:
-                return False
-        return True
-
-    def combi(n, start):
-        global answer
-        if len(c) == n:
-            flag = False
-            per = list(permutations(c, len(c)))
-            for p in per:
-                flag = right(p)
-                for key in ban_check:
-                    ban_check[key] = 0
-                if flag:
+    for com in com_list:
+        per = list(permutations(com, B))
+        for p in per:
+            is_ban = True
+            for i in range(B):
+                if not is_match(p[i], banned_id[i]):
+                    is_ban = False
                     break
 
-            if flag:
+            if is_ban:
                 answer += 1
-            return
-        for i in range(start, len(user_id)):
-            if check[i] == 0:
-                check[i] = 1
-                c.append(user_id[i])
-                combi(n, i+1)
-                check[i] = 0
-                c.pop()
-
-    combi(len(banned_id), 0)
+                break
 
     return answer
