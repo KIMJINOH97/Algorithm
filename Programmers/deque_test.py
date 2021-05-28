38dcabd948144095ebc9a415930377825aec4c94
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-05-28 21:42:06 +0900

    type: deque popleft(), list pop()  시간 비교

--- Programmers/deque_test.py
+++ Programmers/deque_test.py
@@ -3,24 +3,73 @@ from collections import deque
 from math import floor
 import time
 
-N = 10000
+N = 1000000
 
 
 start = time.time()
 queue = [0 for i in range(N)]
 end = time.time()
-print("10000개 넣은 list: ", floor((end-start)*10000), "ms")
+print(N, "개 넣은 list <append(i)>: ", floor((end-start)*10000), "ms")
 
 
 start = time.time()
-deque = deque()
+dq = deque()
 for i in range(N):
-    deque.append(i)
+    dq.append(i)
 end = time.time()
-print("10000개 넣은 deque: ", floor((end-start)*10000), "ms")
+print(N, "개 넣은 deque <append(i)>: ", floor((end-start)*10000), "ms")
+
+li = []
+
+for i in range(N):
+    li.append(i)
+
+start = time.time()
+while li:
+    li.pop()
+
+end = time.time()
+
+print(N, "개 pop 한 list: ", floor((end-start)*10000), "ms")
+
+
+dq = deque()
+
+for i in range(N):
+    dq.appendleft(i)
+
+start = time.time()
+
+while dq:
+    dq.popleft()
+
+end = time.time()
+print(N, "개 popleft 한 deque: ", floor((end-start)*10000), "ms")
+
 
 # li = []
 # print("빈 리스트 : ", getsizeof(li))
 # for i in range(10):
 #     li.append(0)
 #     print(i+1, "번째 원소 삽입 후 크기: ", getsizeof(li))
+
+
+start = time.time()
+
+for i in range(N):
+    li.append(i)
+
+end = time.time()
+
+print(N, "개 append한 list: ", floor((end-start)*10000), "ms")
+
+
+start = time.time()
+
+for i in range(N):
+    dq.appendleft(i)
+
+end = time.time()
+print(N, "개 appendleft 한 deque: ", floor((end-start)*10000), "ms")
+
+start = time.time()
