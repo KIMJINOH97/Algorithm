7548b09a3fc309a800d0e1f21f439ce317f12513
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-07-01 19:01:41 +0900

    feat: 구현

---
+++ java/Programmers/level1/Lotto.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Lotto {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {0, 0};
+        int max_count = 0;
+        int min_count = 0;
+
+        for (int i =0; i<lottos.length; i++){
+            if(lottos[i] == 0){
+                max_count++;
+                continue;
+            }
+
+            for(int j=0; j<win_nums.length; j++){
+                if(win_nums[j] == lottos[i]){
+                    max_count++;
+                    min_count++;
+                }
+            }
+        }
+
+        Map<Integer, Integer> score = new HashMap<>();
+        for(int i=1; i<7; i++){
+            score.put(7-i, i);
+        }
+        score.put(0, 6);
+
+        answer[0] = score.get(max_count);
+        answer[1] = score.get(min_count);
+
+        return answer;
+    }
+}
