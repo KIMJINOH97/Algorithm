7c7f3c13073acba308b450dd17d2e16100cc04c4
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-08-13 17:27:02 +0900

    refactor: 2021 KAKAO BLIND

--- python/Programmers/kakao/with_taxi_fare.py
+++ python/Programmers/kakao/with_taxi_fare.py
@@ -1,40 +1,41 @@
-from heapq import heappush, heappop
-
-INF = 99999999
+from heapq import heappop, heappush
 
 
-def solution(n, s, a, b, fares):
-    answer = INF
-    s, a, b = s-1, a-1, b-1
-    dist = [INF for i in range(n)]
-    road = [[] for i in range(n)]
-    hq = []
+def solution(n, start, a, b, fares):
+    INF = 1000000000
+    edge = [[] for i in range(n)]
+
+    start, a, b = start-1, a-1, b-1
     for fare in fares:
-        road[fare[0]-1].append((fare[1]-1, fare[2]))  # 지점, 거리
-        road[fare[1]-1].append((fare[0]-1, fare[2]))
+        s, e, d = fare
+        s, e = s-1, e-1
+        edge[s].append((e, d))
+        edge[e].append((s, d))
+    q = []
 
-    def dijk(start, distance):
-        heappush(hq, (start, 0))
-        while hq:
-            v, dis = heappop(hq)
-            if dis > distance[v]:
+    def dijkstra(k):
+        dist = [INF for i in range(n)]
+        dist[k] = 0
+        q.append((0, k))
+        while q:
+            d, vertax = heappop(q)
+
+            if d > dist[vertax]:
                 continue
-            for i in range(len(road[v])):
-                new_road = road[v][i][1] + dis
-                if new_road < distance[road[v][i][0]]:
-                    distance[road[v][i][0]] = new_road
-                    heappush(hq, (road[v][i][0], new_road))
-    dist[s] = 0
-    dijk(s, dist)
+
+            for ver in edge[vertax]:
+                end, distance = ver
+                if distance + d < dist[end]:
+                    dist[end] = distance + d
+                    heappush(q, (dist[end], end))
+
+        return dist
+
+    d = dijkstra(start)
+    answer = INF
     for i in range(n):
-        d = [INF for i in range(n)]
-        ans = 0
-        dijk(i, d)
-        if i == a:
-            ans = dist[i] + d[b]
-        elif i == b:
-            ans = dist[i] + d[a]
-        else:
-            ans = dist[i] + d[a] + d[b]
-        answer = min(ans, answer)
+        dis = dijkstra(i)
+        # d[i] 공통으로 타고 간 거리
+        answer = min(answer, d[i] + dis[a] + dis[b])
+
     return answer
