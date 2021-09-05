8f5e82500bfb41532c96dfdc0be0bc161fc3977b
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-09-05 18:10:43 +0900

    type: 시간복잡도 O(nlogn)으로 풀기

--- python/Programmers/kakao/insert_ad.py
+++ python/Programmers/kakao/insert_ad.py
@@ -1,3 +1,6 @@
+from heapq import heappop, heappush
+
+
 def solution(play_time, adv_time, logs):
     answer = [0, 0]
     new_log = []
@@ -46,3 +49,65 @@ def solution(play_time, adv_time, logs):
             index = i-adv_time+1
 
     return int_to_str(index)
+
+
+# PQ 써서 풀기
+
+
+def solution(play_time, adv_time, logs):
+    answer = 0
+
+    def toSecond(time):
+        hour, minute, second = map(int, time.split(':'))
+        return hour * 3600 + minute * 60 + second
+
+    def toClock(time):
+        hour = time // 3600
+        minute = (time - 3600 * hour) // 60
+        second = time - (3600 * hour) - (60 * minute)
+        clock = list(map(str, [hour, minute, second]))
+        st = ""
+        for i in range(len(clock)):
+            if len(clock[i]) == 1:
+                clock[i] = "0" + clock[i]
+        return ":".join(clock)
+
+    play_time, adv_time = toSecond(play_time), toSecond(adv_time)
+    TOTAL_TIME = play_time+1
+    times = []
+    sumOfLog = []
+
+    for log in logs:
+        start, end = map(toSecond, log.split('-'))
+        times.append((start, end))
+
+    times.sort()
+
+    q = []
+    time_index = 0
+    for i in range(TOTAL_TIME):
+        while time_index < len(times) and times[time_index][0] == i:
+            heappush(q, (times[time_index][1]))
+            time_index += 1
+
+        while q and q[0] == i:
+            heappop(q)
+
+        sumOfLog.append(len(q))
+
+    left, right = 0, adv_time-1
+    sumOfAdv = 0
+    for i in range(adv_time+1):
+        sumOfAdv += sumOfLog[i]
+
+    answer = left
+    maxAdv = sumOfAdv
+    while right + 1 < TOTAL_TIME:
+        sumOfAdv -= sumOfLog[left]
+        left += 1
+        right += 1
+        sumOfAdv += sumOfLog[right]
+        if sumOfAdv > maxAdv:
+            maxAdv = sumOfAdv
+            answer = left
+    return toClock(answer)
