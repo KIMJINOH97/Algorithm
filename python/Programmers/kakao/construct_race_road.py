13d6851683a4693e42be623eddbb1da96de858b4
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-09 23:55:29 +0900

    refactor: bfs, 구현 2020 KAKAO INTERN

--- python/Programmers/kakao/construct_race_road.py
+++ python/Programmers/kakao/construct_race_road.py
@@ -1,37 +1,50 @@
+from collections import deque
+
 def solution(board):
-    answer = 0
+    GARO, SERO, WALL = 0, 1, 1
     N = len(board)
+    
+    q = deque()
+    
     nr, nc = [0, 1, 0, -1], [1, 0, -1, 0]
-    check = [[0 for i in range(N)] for j in range(N)]
-    q = []
-
-    def bfs():
-        while q:
-            r, c, d, s = q.pop(0)
-            for i in range(4):
-                dr, dc = r + nr[i], c + nc[i]
-                if 0 <= dr < N and 0 <= dc < N and board[dr][dc] == 0:
-                    if i % 2 != s % 2:
-                        if check[dr][dc] == 0:
-                            check[dr][dc] = d+600
-                            q.append((dr, dc, d+600, i))
-                        elif d+600 <= check[dr][dc]:
-                            check[dr][dc] = d+600
-                            q.append((dr, dc, d+600, i))
-                    else:
-                        if check[dr][dc] == 0:
-                            check[dr][dc] = d+100
-                            q.append((dr, dc, d+100, i))
-                        elif d+100 <= check[dr][dc]:
-                            check[dr][dc] = d+100
-                            q.append((dr, dc, d+100, i))
-        return check[N-1][N-1]
-
+    
+    dist = [[100000000 for i in range(N)] for j in range(N)]
+    dist[0][0] = 0
     if board[0][1] == 0:
-        check[0][1] = 100
-        q.append((0, 1, 100, 0))
+        dist[0][1] = 100
+        q.append((100, 0, 1, GARO))
     if board[1][0] == 0:
-        check[1][0] = 100
-        q.append((1, 0, 100, 1))
-    answer = bfs()
-    return answer
+        dist[1][0] = 100
+        q.append((100, 1, 0, SERO))
+    
+    while q:
+        cost, r, c, where = q.popleft()
+        
+        for i in range(4):
+            dr, dc = r + nr[i], c + nc[i]
+            
+            if dr < 0 or dc < 0 or dr >= N or dc >= N or board[dr][dc] == WALL:
+                continue
+                
+            if cost > dist[dr][dc] + 600:
+                continue
+                
+            if i % 2 == 0: # 가로방향
+                if where == GARO:
+                    dist[dr][dc] = min(dist[dr][dc], cost + 100)
+                    q.append((cost+100, dr, dc, GARO))
+                else:
+                    dist[dr][dc] = min(dist[dr][dc], cost + 600)
+                    q.append((cost+600, dr, dc, GARO))
+            else:
+                if where == SERO:
+                    dist[dr][dc] = min(dist[dr][dc], cost + 100)
+                    q.append((cost+100, dr, dc, SERO))
+                else:
+                    dist[dr][dc] = min(dist[dr][dc], cost + 600)
+                    q.append((cost+600, dr, dc, SERO))
+    
+    for i in range(N):
+        print(dist[i])
+    
+    return dist[N-1][N-1]
